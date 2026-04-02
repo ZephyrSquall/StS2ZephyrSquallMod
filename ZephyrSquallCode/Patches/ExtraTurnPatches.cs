55884fa660972b5b8a2a1123b1332bc3512e69be
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-04-02 22:47:51 +0800

    Fix Burst of Speed thinking it's an extra turn if the last combat was won during an extra turn

--- ZephyrSquallCode/Patches/ExtraTurnPatches.cs
+++ ZephyrSquallCode/Patches/ExtraTurnPatches.cs
@@ -2,6 +2,7 @@ using HarmonyLib;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Runs;
 
 namespace ZephyrSquall.ZephyrSquallCode.Patches;
 
@@ -14,18 +15,29 @@ public class ExtraTurnTracker
 class ExtraTurnStartPatch
 {
     [HarmonyPrefix]
-    static bool TrackExtraTurnStart(CombatState combatState, Player player)
+    static bool TrackExtraTurnExtraTurnStart(CombatState combatState, Player player)
     {
         ExtraTurnTracker.IsExtraTurn = true;
         return true;
     }
 }
 
-[HarmonyPatch(typeof(Hook), nameof(Hook.AfterTurnEnd))]
-class ExtraTurnEndPatch
+[HarmonyPatch(typeof(Hook), nameof(Hook.BeforeCombatStart))]
+class CombatStartPatch
 {
     [HarmonyPrefix]
-    static bool TrackExtraTurnEnd(CombatState combatState, CombatSide side)
+    static bool TrackExtraTurnCombatStart(IRunState runState, CombatState? combatState)
+    {
+        ExtraTurnTracker.IsExtraTurn = false;
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(Hook), nameof(Hook.AfterTurnEnd))]
+class TurnEndPatch
+{
+    [HarmonyPrefix]
+    static bool TrackExtraTurnTurnEnd(CombatState combatState, CombatSide side)
     {
         ExtraTurnTracker.IsExtraTurn = false;
         return true;
