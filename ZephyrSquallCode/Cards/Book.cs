b9ebc21de310c7548ee8ad5fd8b60f2e885a3e73
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-04-17 20:22:09 +0800

    Attempt to preview Recorded cards above a Book

--- ZephyrSquallCode/Cards/Book.cs
+++ ZephyrSquallCode/Cards/Book.cs
@@ -1,15 +1,19 @@
 using System.Text;
 using BaseLib.Abstracts;
+using BaseLib.Patches.UI;
 using BaseLib.Utils;
+using Godot;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.UI;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes.Cards;
 using ZephyrSquall.ZephyrSquallCode.CardPiles;
 
 namespace ZephyrSquall.ZephyrSquallCode.Cards;
@@ -17,15 +21,13 @@ namespace ZephyrSquall.ZephyrSquallCode.Cards;
 [Pool(typeof(TokenCardPool))]
 public class Book() : CustomCardModel(1,
     CardType.Skill, CardRarity.Token,
-    TargetType.Self)
+    TargetType.Self), ICustomUiModel
 {
     public List<CardModel> RecordedCards { get; private set; } = [];
 
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Retain];
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [new StringVar("CardTitles")];
-    
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => RecordedCards.Select(card => HoverTipFactory.FromCard(card));
     
     public static async Task<Book?> CreateInHand(
         Player owner,
@@ -74,5 +76,33 @@ public class Book() : CustomCardModel(1,
             sb.Append($"[gold]{RecordedCards[i].Title}[/gold]");
         }
         ((StringVar) DynamicVars["CardTitles"]).StringValue = sb.ToString();
+    }
+
+    public void CreateCustomUi(Control toAdd)
+    {
+        // Eventually, this method will show a preview for all Recorded cards, but only while hover tips are displaying.
+        // For now, I'm having trouble getting the card preview to display without breaking, so I'm simplifying to just
+        // showing the first  Recorded card above the Book at all times.
+        var firstRecordedCard = RecordedCards.FirstOrDefault();
+        if (firstRecordedCard != null)
+        {
+            Log.Warn("Book has recorded cards!");
+            var nCard = NCard.Create(firstRecordedCard);
+
+            if (nCard is { } nonNullNCard)
+            {
+                toAdd.AddChild(nCard);
+                Log.Warn("Found an NCard for Book's first recorded card!");
+                nCard.Visibility = ModelVisibility.Visible;
+                nCard.Position = new Vector2(0f, -500f);
+                nCard.UpdateVisuals(nCard.Model.Pile.Type, CardPreviewMode.Normal);
+                nCard._Ready();
+                
+                // It seems the card preview never enters a "Ready" state, which UpdateVisuals explicitly checks for and
+                // will do no work if it's not ready. This line helps me check if this may be the case.
+                if (!nCard.IsNodeReady())
+                    Log.Warn("Node is NOT ready!");
+            }
+        }
     }
 }
