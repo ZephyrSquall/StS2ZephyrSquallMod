11c8097644a40c5e36bc448971e25c357d2f5959
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-04-18 00:54:20 +0800

    Add card "Wariness"

---
+++ ZephyrSquallCode/Cards/Wariness.cs
@@ -0,0 +1,38 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace ZephyrSquall.ZephyrSquallCode.Cards;
+
+public class Wariness() : ZephyrSquallCard(1,
+    CardType.Skill, CardRarity.Common,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(6M, ValueProp.Move)];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+    
+    public override async Task BeforeHandDraw(
+        Player player,
+        PlayerChoiceContext choiceContext,
+        CombatState combatState)
+    {
+        if (Pile.Type == PileType.Discard && player == Owner)
+            await CardPileCmd.Add(this, PileType.Hand);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(2M);
+}
