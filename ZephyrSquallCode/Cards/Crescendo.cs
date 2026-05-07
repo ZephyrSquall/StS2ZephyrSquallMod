96181fbbbc3a600b25a9001fb889d0a37f339952
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-05-07 20:53:45 +0800

    Add card "Crescendo"

---
+++ ZephyrSquallCode/Cards/Crescendo.cs
@@ -0,0 +1,27 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using ZephyrSquall.ZephyrSquallCode.Powers;
+using ZephyrSquall.ZephyrSquallCode.Utilities;
+
+namespace ZephyrSquall.ZephyrSquallCode.Cards;
+
+public class Crescendo() : ZephyrSquallCard(1,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<CrescendoPower>(2)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [ZephyrHoverTips.Honed()];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<CrescendoPower>(choiceContext, Owner.Creature, DynamicVars["CrescendoPower"].IntValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade() => DynamicVars["CrescendoPower"].UpgradeValueBy(1);
+}
---
+++ ZephyrSquallCode/Powers/CrescendoPower.cs
@@ -0,0 +1,26 @@
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using ZephyrSquall.ZephyrSquallCode.Commands;
+using ZephyrSquall.ZephyrSquallCode.Utilities;
+
+namespace ZephyrSquall.ZephyrSquallCode.Powers;
+
+public sealed class CrescendoPower : ZephyrSquallPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [ZephyrHoverTips.Honed()];
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var card = cardPlay.Card;
+        if (card.Owner.Creature == Owner && card.Type == CardType.Attack)
+        {
+            Flash();
+            await ModifierCmd.AddHonedToSpecific(card, Amount, this);
+        }
+    }
+}
