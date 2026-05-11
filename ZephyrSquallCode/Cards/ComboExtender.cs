239f2e469191fed622d3cf531ea699cb901aa232
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-05-11 20:06:31 +0800

    Add card "Combo Extender"

---
+++ ZephyrSquallCode/Cards/ComboExtender.cs
@@ -0,0 +1,27 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using ZephyrSquall.ZephyrSquallCode.Utilities;
+
+namespace ZephyrSquall.ZephyrSquallCode.Cards;
+
+public class ComboExtender() : ZephyrSquallCard(0, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override HashSet<CardTag> CanonicalTags => [ZephyrCardTags.Combo];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(3, ValueProp.Move), new RepeatVar(2)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(DynamicVars.Repeat.IntValue)
+            .FromCard(this)
+            .Targeting(play.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Repeat.UpgradeValueBy(1);
+}
