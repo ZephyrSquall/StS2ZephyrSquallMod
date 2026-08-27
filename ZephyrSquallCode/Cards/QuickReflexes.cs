ceebcd72de5bb2b6b93c2e015841ed99fe3c029c
Zephyr Squall <145758512+ZephyrSquall@users.noreply.github.com>
2026-08-28 00:58:38 +0800

    Add card "Quick Reflexes"

---
+++ ZephyrSquallCode/Cards/QuickReflexes.cs
@@ -0,0 +1,28 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace ZephyrSquall.ZephyrSquallCode.Cards;
+
+public class QuickReflexes() : ZephyrSquallCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(7, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
+    }
+
+    public override async Task AfterCardDrawnEarly(PlayerChoiceContext choiceContext, CardModel card, bool fromHandDraw)
+    {
+        if (card == this && !fromHandDraw && CombatState.CurrentSide == Owner.Creature.Side)
+            await CardCmd.AutoPlay(choiceContext, card, null);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(3);
+}
